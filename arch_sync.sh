9841236d5e1ef863dfe260b1286b2881e3f8aa51
Valeh Agayev <valeh.agayev@gmail.com>
2017-02-22 22:31:02 +0400

    Update arch_sync.sh

--- arch_sync.sh
+++ arch_sync.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 . /home/oracle/.bash_profile
+export ORACLE_SID=+ASM1  
+export ORACLE_HOME=$GRID_HOME  
+export PATH=$ORACLE_HOME/bin:$BASE_PATH  
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib  
+export CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib 
 cd /home/oracle/backup/rman/archivelogs/
 mkdir `date -d "1 day ago" "+%Y_%m_%d"`
-. /home/oracle/grid_env
 for i in $(asmcmd ls +FRA/TESTDB/ARCHIVELOG/`date -d "1 day ago" "+%Y_%m_%d"`);
 do asmcmd cp +FRA/TESTDB/ARCHIVELOG/`date -d "1 day ago" "+%Y_%m_%d"`/$i /home/oracle/backup/rman/archivelogs/`date -d "1 day ago" "+%Y_%m_%d"`;
 done
